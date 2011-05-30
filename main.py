b89a8caf48cd4fa7bd1dd9ab9d2d0ae114e824d6
Chris Vaughn <chris.vaughn@lifechurch.tv>
2011-05-30 10:46:11 -0500

    more organization
    trying to figure out more about python

--- main.py
+++ main.py
@@ -15,54 +15,15 @@
 # limitations under the License.
 #
 
-import os
-import base64
-import logging
-import uuid
-
-from google.appengine.ext import db
 from google.appengine.ext import webapp
-from google.appengine.api import users as google_users
 from google.appengine.ext.webapp import util
-from google.appengine.ext.webapp import template
 
-from users import Users
-
-TOKEN_LENGTH = 10
-
-class MainHandler(webapp.RequestHandler):
-    """ handler for / """
-    def get(self):
-        """ this handler supports http get """
-        data = {}
-        if google_users.get_current_user():
-            google_user = google_users.get_current_user()
-            user = db.Query(Users).filter('user_id =', google_user.user_id()).get()
-            if user is None:
-                token = generate_token(TOKEN_LENGTH)
-                user = Users(user_id = google_user.user_id(), email = google_user.email(), token = token)
-                db.put(user)
-
-            data['url'] = google_users.create_logout_url(self.request.uri)
-            data['user'] = google_users.get_current_user()
-            data['user_token'] = user.token
-
-        else:
-            data['url'] = google_users.create_login_url(self.request.uri)
-
-        path = os.path.join(os.path.dirname(__file__), 'home.html')
-        self.response.out.write(template.render(path, data))
-
-
-def generate_token(token_length):
-    u = uuid.uuid4()
-    return u.bytes.encode("base64")[:token_length]
+from controller.home import HomeController
 
 def main():
-    application = webapp.WSGIApplication([('/', MainHandler)],
+    application = webapp.WSGIApplication([('/', HomeController)],
                                          debug=True)
     util.run_wsgi_app(application)
-
 
 if __name__ == '__main__':
     main()
--- users.py
+++
@@ -1,7 +0,0 @@
-""" models module contains the App Engine DataStore models """
-from google.appengine.ext import db
-
-class Users(db.Model):
-    user_id = db.StringProperty()
-    email = db.StringProperty()
-    token = db.StringProperty()
